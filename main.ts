cfc47ff78e9b9d817c36e9b5c3a6b36e089508c3
s21110 <122764722+s21110@users.noreply.github.com>
2023-09-15 11:08:26 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let X = 0
 basic.forever(function () {
-	
+    X = 0
+    for (let index = 0; index < 5; index++) {
+        basic.clearScreen()
+        led.plot(X, 0)
+        basic.pause(100)
+        X += 1
+    }
 })
